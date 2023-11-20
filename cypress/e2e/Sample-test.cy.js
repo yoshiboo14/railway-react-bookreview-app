5f9428dbc3657bb16618748441d47c37f5f3937b
yoshiboo <yoshikikaitech1414@gmail.com>
2023-11-20 13:08:57 +0900

    cypressのテスト用ファイルの作成

---
+++ cypress/e2e/Sample-test.cy.js
@@ -0,0 +1,12 @@
+describe("Sample test", () => {
+  it("check data", () => {
+    // cyは
+    cy.visit("test_cypress.html"); // 今回はファイルの相対パス
+
+    cy.get("#input-email").type("dummy@email.com"); // 適当なメールアドレスを渡す
+    cy.get("#select-role").select("管理者"); //権限の選択
+    cy.get("#submit").click(); // 登録ボタンを押す
+    cy.get("#result-email").should("have.text", "dummy@email.com"); // 以下、出力のチェック
+    cy.get("#result-role").should("have.text", "管理者");
+  });
+});
