e580c0a508564e5f5f0cb3be60ddeb0b6643479f
yoshiboo <yoshikikaitech1414@gmail.com>
2023-11-21 10:36:11 +0900

    ログイン画面の簡易的なUIの作成

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const SignIn = () => {
-  return <div>ログイン画面</div>;
+  const signInStyle = {
+    alignItems: "center",
+    textAlign: "center",
+    paddingTop: "100px",
+  };
+
+  return (
+    <>
+      <form action="#" method="post" style={signInStyle}>
+        <h1>ログイン画面</h1>
+        <label htmlFor="name">
+          <p>ユーザー名</p>
+          <input type="text" name="name" id="name" placeholder="名前を入力" />
+        </label>
+        <label htmlFor="email">
+          <p>メールアドレス</p>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="メールアドレスを入力"
+          />
+        </label>
+        <label htmlFor="password">
+          <p>パスワード</p>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="パスワードを入力"
+          />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="ログインする" />
+        <br />
+        <br />
+        <Link to="/signUp">まだ新規登録ができてない場合</Link>
+      </form>
+    </>
+  );
 };
