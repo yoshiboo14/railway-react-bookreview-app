51fd719ded800c00f203ae23201468f9ac73b805
yoshiboo <yoshikikaitech1414@gmail.com>
2023-11-21 10:20:19 +0900

    新規作成画面の簡易的なUI作成

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,5 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const SignUp = () => {
-  return <div>新規作成画面</div>;
+  const signUpStyle = {
+    alignItems: "center",
+    textAlign: "center",
+    paddingTop: "100px",
+  };
+
+  return (
+    <>
+      <form action="#" method="post" style={signUpStyle}>
+        <h1>新規作成画面</h1>
+        <label htmlFor="name">
+          <p>ユーザー名</p>
+          <input type="text" name="name" id="name" placeholder="名前を入力" />
+        </label>
+        <label htmlFor="email">
+          <p>メールアドレス</p>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="メールアドレスを入力"
+          />
+        </label>
+        <label htmlFor="password">
+          <p>パスワード</p>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="パスワードを入力"
+          />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="新規作成する" />
+        <br />
+        <br />
+        <Link to="/signIn">すでに登録済みの方</Link>
+      </form>
+    </>
+  );
 };
