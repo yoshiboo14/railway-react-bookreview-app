8a099774f6e7817a25a8ff6cdf1da4f7566189d4
yoshiboo <yoshikikaitech1414@gmail.com>
2023-11-23 11:26:49 +0900

    ファイル名変更

--- src/jest_test/signinjest.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import render from "@testing-library/react";
-import { SignIn } from "../SignIn";
-
-describe("ログインフォームのテストを行います", () => {
-  test("サインインコンポーネントをレンダリング", () => {
-    render(<SignIn />);
-  });
-});
