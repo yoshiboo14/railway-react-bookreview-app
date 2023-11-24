d499be95d12e138d2963e30183faf69749d36bc4
yoshiboo <yoshikikaitech1414@gmail.com>
2023-11-24 15:05:16 +0900

    正しい入力と誤った入力で場合分け

--- cypress/e2e/SignInTest.cy.js
+++ cypress/e2e/SignInTest.cy.js
@@ -1,17 +1,18 @@
 describe("Sample test", () => {
   it("check data", () => {
-    cy.visit("signintestcy.html"); // 今回はファイルの相対パス
+    cy.visit("signintestcy.html"); //ファイルの相対パス
 
     cy.get("#input-name").type("ログイン太郎");
     cy.get("#input-email").type("test.test@email.com"); // 適当なメールアドレスを渡す
     cy.get("#input-password").type("PasswordSample123");
     cy.get("#submit").click(); // ログインボタンを押す
 
-    // 以下、出力のチェック
+    // 出力のチェック
     cy.get("#result-name").should("have.text", "ログイン太郎");
-    cy.get("#result-email").should("have.text", "test.test@email.com");
+    // 正しい入力
+    // cy.get("#result-email").should("have.text", "test.test@email.com");
+    // 間違った入力の場合
+    cy.get("#result-email").should("have.text", "dummy@email.com");
     cy.get("#result-password").should("have.text", "PasswordSample123");
-
-    // cy.get("#result-email").should("have.text", "dummy@email.com");
   });
 });
