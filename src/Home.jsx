fc9a781d7af8ec5e000db9442b5e1d6be117eda8
yoshiboo <yoshikikaitech1414@gmail.com>
2023-11-28 23:48:37 +0900

    書籍一覧取得の非同期処理を追加(未完成)

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
+  // 書籍一覧を取得
+  useEffect(() => {
+    axios
+      .get("https://railway.bookreview.techtrain.dev/books")
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="App">
-      <h1>ホーム画面</h1>
-      {/* <p>email</p>
-      <input type="email" />
-      <br />
-      <p>password</p>
-      <input type="password" />
-      <br /> */}
+      <h1>書籍レビューの一覧画面</h1>
       <Link to="/signUp">
         <button>新規作成する</button>
       </Link>
