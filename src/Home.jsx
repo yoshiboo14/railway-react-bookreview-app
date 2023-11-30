3c0624a2142afcf6dbccb4ac8102c237e7debdbe
yoshiboo <yoshikikaitech1414@gmail.com>
2023-11-30 14:37:21 +0900

    Pagenationコンポーネントのインポートと次へボタン前へボタンの追加

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Pagination } from "./Pagination";
 
 export const Home = () => {
   // 書籍一覧を取得
@@ -11,17 +12,28 @@ export const Home = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  // 次へボタン
+  const nextPage = () => {};
+
+  // 前へボタン
+  const prevPage = () => {};
+
   return (
-    <div className="App">
-      <h1>書籍レビューの一覧画面</h1>
-      <Link to="/signUp">
-        <button>新規作成する</button>
-      </Link>
-      <br />
-      <br />
-      <Link to="/signIn">
-        <button>ログインする</button>
-      </Link>
-    </div>
+    <>
+      <div className="App">
+        <h1>書籍レビューの一覧画面</h1>
+        <Link to="/signUp">
+          <button>新規作成する</button>
+        </Link>
+        <br />
+        <br />
+        <Link to="/signIn">
+          <button>ログインする</button>
+        </Link>
+      </div>
+      <Pagination />
+      <button onClick={prevPage}>前へ</button>
+      <button onClick={nextPage}>次へ</button>
+    </>
   );
 };
