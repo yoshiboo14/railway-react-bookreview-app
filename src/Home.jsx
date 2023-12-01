e6c44ab4e8d52586266829bedc1fc7b84af34c9a
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-01 13:24:06 +0900

    ローカルストレージに保存したトークンを取得する機能を追加、書籍一覧を取得成功、取得した10件の配列をステートで管理、jsxでレビューを展開

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,28 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Pagination } from "./Pagination";
 
 export const Home = () => {
+  // ローカルストレージに保存したトークンを取得する
+  const accessToken = localStorage.getItem("accessToken");
+
+  // 取得した10件の配列をステートで管理
+  const [reviews, setReviews] = useState("");
+
   // 書籍一覧を取得
   useEffect(() => {
     axios
-      .get("https://railway.bookreview.techtrain.dev/books")
-      .then((res) => console.log(res))
+      .get("https://railway.bookreview.techtrain.dev/books", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setReviews(res.data);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -31,6 +45,20 @@ export const Home = () => {
           <button>ログインする</button>
         </Link>
       </div>
+      {/* レビューを展開 */}
+      {reviews.map((review) => {
+        return (
+          <>
+            <h1 key={review.id}>{review.title}</h1>
+            <h2 key={review.id}>{review.review}</h2>
+            <ul>
+              <li key={review.id}>詳細　{review.detail}</li>
+              <li key={review.id}>投稿者　{review.reviewer}</li>
+              <li key={review.id}>URL {review.url}</li>
+            </ul>
+          </>
+        );
+      })}
       <Pagination />
       <button onClick={prevPage}>前へ</button>
       <button onClick={nextPage}>次へ</button>
