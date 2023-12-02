47288c2c73a17992b4ac2180b042de917afe7f78
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-02 10:36:07 +0900

    Booksコンポーネントのルーティング設定

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { SignUp } from "./SignUp";
 import { SignIn } from "./SignIn";
-import { LogIn } from "./LogIn_jest";
+// import { LogIn } from "./LogIn_jest";
+import { Books } from "./Books";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/signUp" element={<SignUp />}></Route>
         <Route path="/signIn" element={<SignIn />}></Route>
-        <Route path="/LogIn" element={<LogIn />}></Route>
+        {/* <Route path="/LogIn" element={<LogIn />}></Route> */}
+        <Route path="/books" element={<Books />}></Route>
       </Routes>
     </BrowserRouter>
   );
