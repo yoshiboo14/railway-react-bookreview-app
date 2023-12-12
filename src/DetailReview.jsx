cb4aff28258fce999fdcb117727f02bd74b59a32
yoshiboo <yoshikikaitech1414@gmail.com>
2023-12-12 15:57:49 +0900

    書籍詳細一覧を作成

---
+++ src/DetailReview.jsx
@@ -0,0 +1,108 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { useParams } from "react-router-dom";
+
+export const DetailReview = () => {
+  // レビューのid
+  const { id } = useParams();
+  // レビュー情報
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [detail, setDetail] = useState("");
+  const [review, setReview] = useState("");
+
+  //   console.log(id);
+
+  const [accessToken, setAccessToken] = useCookies();
+
+  // スタイル
+  const Review = {
+    alignItems: "center",
+    textAlign: "center",
+    paddingTop: "100px",
+  };
+
+  //   const id = "e3e1eabf-5671-4333-8075-35f6fc5e8141";
+
+  useEffect(() => {
+    axios
+      .get(`https://railway.bookreview.techtrain.dev/books/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken.token}`,
+        },
+      })
+      .then((res) => {
+        setTitle(res.data.title);
+        setUrl(res.data.url);
+        setDetail(res.data.detail);
+        setReview(res.data.review);
+      })
+      .catch((err) => console.log(err));
+  });
+
+  return (
+    <>
+      <form method="post" style={Review}>
+        <h1>レビュー詳細画面</h1>
+        <label htmlFor="review">
+          <p>レビュー</p>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="タイトル"
+            value={title}
+            onChange={(e) => {
+              //   setTitle(e.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <br />
+        <input
+          type="text"
+          name="url"
+          id="url"
+          placeholder="URL"
+          value={url}
+          onChange={(e) => {
+            // setUrl(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <textarea
+          type="text"
+          name="detail"
+          id="detail"
+          placeholder="詳細内容"
+          value={detail}
+          onChange={(e) => {
+            // setDetail(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <textarea
+          type="text"
+          name="review"
+          id="review"
+          placeholder="新しいレビュー"
+          value={review}
+          onChange={(e) => {
+            // setReview(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        <button
+          type="button"
+          // onClick={onAddReview}
+        >
+          レビューを編集する
+        </button>
+      </form>
+    </>
+  );
+};
